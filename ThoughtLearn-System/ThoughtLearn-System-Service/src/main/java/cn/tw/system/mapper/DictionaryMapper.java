df187acf4e6b40049e43f1be7069a196b448a7c7
nanyidev <1583881538@qq.com>
2023-07-28 23:52:09 +0800

    add system module

---
+++ ThoughtLearn-System/ThoughtLearn-System-Service/src/main/java/cn/tw/system/mapper/DictionaryMapper.java
@@ -0,0 +1,16 @@
+package cn.tw.system.mapper;
+
+import cn.tw.system.model.po.Dictionary;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * <p>
+ * 数据字典 Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface DictionaryMapper extends BaseMapper<Dictionary> {
+
+}
---
+++ ThoughtLearn-System/ThoughtLearn-System-Service/src/main/java/cn/tw/system/service/DictionaryService.java
@@ -0,0 +1,31 @@
+package cn.tw.system.service;
+
+import cn.tw.system.model.po.Dictionary;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+
+import java.util.List;
+
+/**
+ * <p>
+ * 数据字典 服务类
+ * </p>
+ *
+ * @author itcast
+ * @since 2021-10-07
+ */
+public interface DictionaryService extends IService<Dictionary> {
+
+    /**
+     * 查询所有数据字典内容
+     * @return
+     */
+    List<Dictionary> queryAll();
+
+    /**
+     * 根据code查询数据字典
+     * @param code -- String 数据字典Code
+     * @return
+     */
+    Dictionary getByCode(String code);
+}
